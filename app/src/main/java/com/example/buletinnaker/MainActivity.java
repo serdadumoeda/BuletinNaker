736055f4b4e72f9b84a6655bfdf56f34131c9da2
arif budi setiawan <arifbudi.setiawan@gmail.com>
2019-04-22 11:01:47 +0700

    Tampilan Beranda dengan cardview

--- app/src/main/java/com/example/buletinnaker/MainActivity.java
+++ app/src/main/java/com/example/buletinnaker/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.buletinnaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
 
@@ -12,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this, "firebase success",Toast.LENGTH_LONG).show();
+    }
+
+    public void menu1 (View view){
+        Intent intent = new Intent(MainActivity.this, MenuSatu.class);
+        startActivity(intent);
+    }
+    public void menu2 (View view){
+        Intent intent = new Intent(MainActivity.this, MenuDua.class);
+        startActivity(intent);
     }
 }
